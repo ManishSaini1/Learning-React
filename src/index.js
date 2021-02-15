b0c92c597936d00e78966fb4366110acf985615f
Manish Saini <manish.saini@edstem.com>
2021-02-15 21:51:19 +0530

    Handling events

--- src/index.js
+++ src/index.js
@@ -2,49 +2,77 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
- class Clock extends React.Component
- {
-   constructor(props)
-   {
-     super(props);
-     this.state={date: new Date() }
-   }
+//  class Clock extends React.Component
+//  {
+//    constructor(props)
+//    {
+//      super(props);
+//      this.state={date: new Date() }
+//    }
    
-   componentDidMount()
-   {
-     this.timerID=setInterval(
-       ()=>
-       {
-         this.tick() 
-       },1000
-     )
-      }
-     compnonentWillUnmount()
-     {
-       clearInterval(this.timerID);
-     }
-     tick()
-     {
-       this.setState(
-         {
-           date: new Date()
-         });
-     }
-    //  this.setState={date: Date.now()}
+//    componentDidMount()
+//    {
+//      this.timerID=setInterval(
+//        ()=>
+//        {
+//          this.tick() 
+//        },1000
+//      )
+//       }
+//      compnonentWillUnmount()
+//      {
+//        clearInterval(this.timerID);
+//      }
+//      tick()
+//      {
+//        this.setState(
+//          {
+//            date: new Date()
+//          });
+//      }
+//     //  this.setState={date: Date.now()}
    
-   render()
-   {
-     return(
-      <div>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-     );
-   }
+//    render()
+//    {
+//      return(
+//       <div>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//      );
+//    }
   
- }
+//  }
+class Toggle extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state=
+    {
+      isToggleOn: true
+    }
+    this.handleClick=this.handleClick.bind(this);
+  }
+  handleClick()
+  {
+    this.setState(state=>({
+      isToggleOn : !state.isToggleOn
+    })
+    )
+  }
+  render()
+  {
+      return(
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? "Yes"  : "NO"}
+        </button>
+      )
+  }
+}
 
  ReactDOM.render(
-  <Clock />,
+  // <Clock />,
+  <Toggle/>,
   document.getElementById('root')
 );
 
